196b6d2763e2cb7309bd266d153edbec7817e394
Jose Meléndez <josemelnet@gmail.com>
2020-09-14 23:15:14 -0500

    quotes.py was modified to follow links

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -3,12 +3,17 @@ import scrapy
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+# Next Page Button =  //ul[@class="pager"]//li[@class="next"]/a/@href
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
         'http://quotes.toscrape.com/page/1/'
     ]
+    custom_settings = {
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
 
     def parse(self, response):
 
@@ -21,3 +26,7 @@ class QuotesSpider(scrapy.Spider):
             'quotes': quotes,
             'top_ten_tags': top_ten_tags
         }
+
+        next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback=self.parse)
