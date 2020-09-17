735cde96d61bc36c6fc5203f0b1913baf340498e
Jose Meléndez <josemelnet@gmail.com>
2020-09-16 21:43:30 -0500

    Multiple Callbacks

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,9 +1,10 @@
 import scrapy
 
-# Titulo = //h1/a/text()
+# Título = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
-# Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
-# Next Page Button =  //ul[@class="pager"]//li[@class="next"]/a/@href
+# Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+# Next page button = //ul[@class="pager"]//li[@class="next"]/a/@href
+
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -12,21 +13,48 @@ class QuotesSpider(scrapy.Spider):
     ]
     custom_settings = {
         'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json'
+        'FEED_FORMAT': 'json',
+        'CONCURRENT_REQUESTS': 24,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['facundo@platzi.com'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'PepitoMartinez',
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
+    def parse_only_quotes(self, response, **kwargs):
+        if kwargs:
+            quotes = kwargs['quotes']
+        quotes.extend(response.xpath(
+            '//span[@class="text" and @itemprop="text"]/text()').getall())
+
+        next_page_button_link = response.xpath(
+            '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+        else:
+            yield {
+                'quotes': quotes
+            }
+
     def parse(self, response):
-
         title = response.xpath('//h1/a/text()').get()
-        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        quotes = response.xpath(
+            '//span[@class="text" and @itemprop="text"]/text()').getall()
+        top_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall(
+        )
 
-        yield{
+        top = getattr(self, 'top', None)
+        if top:
+            top = int(top)
+            top_tags = top_tags[:top]
+
+        yield {
             'title': title,
-            'quotes': quotes,
-            'top_ten_tags': top_ten_tags
+            'top_tags': top_tags
         }
 
-        next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        next_page_button_link = response.xpath(
+            '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse)
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
